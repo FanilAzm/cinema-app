c99e7fffe675ea5dac7f9c3ad0817d77c288788c
FanilAzm <fanil.azmagulov@mail.ru>
2023-02-10 12:18:07 +0400

    indicated correct api url

--- next.config.js
+++ next.config.js
@@ -10,11 +10,11 @@ const nextConfig = {
 		return [
 			{
 				source: '/api/:path*',
-				destination: `http://localhost:4200/api/:path*`,
+				destination: `https://cinema-api-fanilazm.vercel.app/api/:path*`,
 			},
 			{
 				source: '/uploads/:path*',
-				destination: `http://localhost:4200/uploads/:path*`,
+				destination: `https://cinema-api-fanilazm.vercel.app/uploads/:path*`,
 			},
 		]
 	},
